ed3863472cfdd1f5d015222d26b81fa8f88979b0
dzakimaulana <dzakimaulanapati@gmail.com>
2024-07-24 11:03:32 +0700

    try to fix lxc-manager

--- Backend/lxc-manager/app/__init__.py
+++ Backend/lxc-manager/app/__init__.py
@@ -7,7 +7,17 @@ app.config.from_object(Config)
 
 CORS(app)
 
+if Config.conn is None:
+    raise Exception("[!] Failed to connect to the database")
+
 cur = Config.conn.cursor()
 
 from app.lxc.routes import lxc_bp
 app.register_blueprint(lxc_bp, url_prefix='/proxmox/lxc')
+
+@app.teardown_appcontext
+def close_db(error):
+    if cur:
+        cur.close()
+    if Config.conn:
+        Config.conn.close()
